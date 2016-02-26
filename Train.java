76b6c363322eb1db579e0e3cc3891f6b648741e1
anirudhc3 <anirudhc3@gmail.com>
2016-02-26 13:44:11 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -2,6 +2,16 @@ public class Train
 {
   public void addCar(String s, Color c)
   {
+    RailCar rc;
+    if(s == "Locomotive")
+    {
+      rc = new Locomotive();
+    }
+    
+    if(s == "PassengerCar")
+    {
+      rc = new PassengerCar();
+    }
     
   }
 }
