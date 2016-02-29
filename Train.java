4746035d44bae0b538882515644336158fb2e62d
anirudhc3 <anirudhc3@gmail.com>
2016-02-28 21:16:38 -0500

    Update Train.java

--- Train.java
+++ Train.java
@@ -1,31 +1,54 @@
+import java.awt.*;
+import java.applet.*;
+import java.util.ArrayList;
+
+
 public class Train
 {
+  ArrayList<RailCar> railCars = new ArrayList<RailCar>();
+  int lastX;
+  int yPos = 150;
+  
   public void addCar(String s, Color c)
   {
-    RailCar rc;
+    
+    RailCar rc;     
     if(s.equals("Locomotive"))
     {
-      rc = new Locomotive();
+      rc = new Locomotive(c,lastX, yPos);
       railCars.add(rc);
     }
     if(s.equals("PassengerCar"))
     {
-      rc = new PassengerCar();
+      rc = new PassengerCar(c,lastX,yPos);
       railCars.add(rc);
     }
     if(s.equals("FreightCar"))
     {
-      rc = new FreightCar();
+      rc = new FreightCar(c,lastX,yPos);
       railCars.add(rc);
     }
     if(s.equals("Caboose"))
     {
-      rc = new Caboose();
+      rc = new Caboose(c,lastX,yPos);
       railCars.add(rc);
     }
-    ArrayList<RailCar> railCars = new ArrayList<RailCar>();
     
+    lastX += 150 + 25;//Car length plus link length
   }
+  
+   public Train(int xStart, int yStart)//Constructors initialize these variables
+   {
+      lastX = xStart;
+      yPos = yStart;     
+   }
+   
+   public void showCars(Graphics g)
+   {
+      for(RailCar r : railCars)
+      {
+         r.drawCar(g);
+      }
+   }
+   
 }
-
-//In main method, put objects of train car types in specific order
